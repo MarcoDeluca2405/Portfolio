57b2ef68fecb1ec709dbbf150706c79cab45ddd5
MarcoDeluca2405 <Marco_de_luca2405@hotmail.com>
2024-12-01 22:47:33 +0100

    improve: add new component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './Components/NavBar/NavBar'
+import HeroSection from './Components/HeroSection/HeroSection'
+import styled from 'styled-components'
+
+
+const DivCenter = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`
 
 function App() {
  
@@ -10,6 +20,10 @@ function App() {
   return (
     <>
       <NavBar />
+      <DivCenter>
+
+      <HeroSection />
+      </DivCenter>
     </>
   )
 }
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,110 +1,176 @@
 import gsap from "gsap";
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import { IStyledComponentBase } from "styled-components/dist/types";
 
 
-const RetNav = styled.div({
-    width:"100vw",
-    height:"50vh",
-    borderBottomLeftRadius:"25px",
-    borderBottomRightRadius:"25px",
-    background: `linear-gradient(
-        180deg,
-        #000000 0%,
-        #000000 1%,
-        #2D2A2A 90%
-      )`,
-      position:"relative",
-      display:"flex",
-      justifyContent:"center"
-});
+const BackGround = styled.div`
+    width: 100vw;
+    height: 60vh;
+    position: absolute;
+    bottom: 0;
+    z-index: -2;
+    background-color: #444242;
+`
 
-const OvalNav = styled.div({
-    width:"125px",
-    height:"115px",
-    borderRadius:"100%",
-    position:"absolute",
-    bottom:-50,  
-    cursor:"pointer"
-});
+const RetNav = styled.div`
+  width: 100vw;
+  height: 50vh;
+  border-bottom-left-radius: 25px;
+  border-bottom-right-radius: 25px;
+  background: linear-gradient(180deg, #000000 0%, #000000 1%, #2D2A2A 90%);
+  position: relative;
+  display: flex;
+  justify-content: center;
+  z-index: 2;
+`;
 
-const OvalImage = styled.img({
-    width:"100%",
-    height:"100%",
-    objectFit:"cover",
-    borderRadius:"50%"
-})
+const OvalNav = styled.div`
+  width: 100px;
+  height: 95px;
+  border-radius: 100%;
+  position: absolute;
+  z-index: 3;
+  bottom: -50px;
+  cursor: pointer;
 
 
-const NavBar:React.FC= () =>{
+  @media screen and (max-width: 768px){
+    width: 70px;
+    height: 65px;
+    bottom: -30px;
+  }
 
-const retNavRef = useRef <HTMLDivElement>(null);
-const ovalNavRef = useRef <HTMLDivElement>(null);
-
-
-
-useEffect(()=>{
-const handleMouseEnter = ()=>{
-if(ovalNavRef.current){
-    gsap.to(retNavRef.current,{
-        boxShadow:"0 10px 40px 5px rgba(128, 0, 128, 0.6)",
-        duration:0.3,
-        yoyo: true,
-        repeat: -1,
-        ease: "power1.inOut",
-    });
-    gsap.to(ovalNavRef.current,{
-        boxShadow:"0 20px 50px 1px rgba(128, 0, 128, 0.6)",
-        duration:0.3,
-        yoyo: true,
-        repeat: -1,
-        ease: "power1.inOut",
-    })
-}
+  @media screen and (max-width:992px){
+    width: 80px;
+    height: 75px;
+    bottom: -30px;
 }
 
-const handleMouseLeave = ()=>{
-    if(ovalNavRef.current){
+`;
+
+const ShadowOval = styled.div<{ isVisible: boolean }>`
+  position: absolute;
+  width: 100px;
+  height: 95px;
+  border-radius: 100%;
+  z-index: -1;
+  top: 49%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  visibility: ${({ isVisible }) => (isVisible ? "visible" : "hidden")};
+  transition: visibility 0.3s ease;
+
+  
+  @media screen and (max-width: 768px){
+    width: 70px;
+    height: 65px;
+    bottom: -30px;
+  }
+
+  @media screen and (max-width:992px){
+    width: 80px;
+    height: 75px;
+    bottom: -30px;
+}
+
+`;
+
+const OvalImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 50%;
+`;
+
+const NavBar: React.FC = () => {
+  const retNavRef = useRef<HTMLDivElement>(null);
+  const ovalNavRef = useRef<HTMLDivElement>(null);
+  const ovalShadowRef = useRef<HTMLDivElement>(null);
+  const backgroundRef = useRef<HTMLDivElement>(null);
+
+  const [isClicked, setIsClicked] = useState(false);
+  const [isShadowVisible, setIsShadowVisible] = useState(true);
+
+  useEffect(() => {
+    let gsapAnimation: GSAPTween | null = null;
+
+    const handleMouseEnter = () => {
+      if (ovalNavRef.current && !isClicked) {
+        gsapAnimation = gsap.to([retNavRef.current, ovalShadowRef.current], {
+          boxShadow: "0 10px 40px 5px rgba(128, 0, 128, 0.6)",
+          duration: 1,
+          yoyo: true,
+          repeat: -1,
+          ease: "power1.inOut",
+        });
+      }
+    };
+
+    const handleMouseLeave = () => {
+      if (gsapAnimation) {
+        gsapAnimation.reverse().eventCallback("onComplete", () => {
+          gsapAnimation?.kill();
+          gsap.to([retNavRef.current, ovalShadowRef.current], {
+            clearProps: "all",
+          });
+        });
+      }
+    };
+
+    const reduceRetNav = () =>{
         gsap.to(retNavRef.current, {
-            boxShadow: "none",
-            duration: 0.3,
-            yoyo: false,
-            repeat: 0,
-           
+            height: "8vh",
+            duration: 1, 
+            delay: 0.5, 
           });
-          gsap.to(ovalNavRef.current, {
-            boxShadow: "none",
-            duration: 0.3,
-            yoyo: false,
-            repeat: 0,
-          });
+
+        gsap.to(backgroundRef.current,{
+            height:"0vh",
+            duration: 1,
+            delay:0.5,
+            onComplete: ()=>{
+                backgroundRef.current?.setAttribute("display","none")
+            }
+        })
+          
     }
-}
 
-const ovaNavElm = ovalNavRef.current;
+    const handleClick = () => {
+      setIsClicked(true); 
+      setIsShadowVisible(false);
+      handleMouseLeave();
+      reduceRetNav(); 
+    };
 
-if(ovaNavElm){
-    ovaNavElm.addEventListener("mouseenter",handleMouseEnter);
-    ovaNavElm.addEventListener("mouseleave",handleMouseLeave);
-}
+    const ovaNavElm = ovalNavRef.current;
 
-},[]);
+    if (ovaNavElm && !isClicked) {
+      ovaNavElm.addEventListener("mouseenter", handleMouseEnter);
+      ovaNavElm.addEventListener("mouseleave", handleMouseLeave);
+      ovaNavElm.addEventListener("click", handleClick); // Gestione del clic
+    }
 
-return(
-<>
-    <RetNav ref={retNavRef}>
+    // Cleanup degli event listeners quando il componente viene smontato
+    return () => {
+      if (ovaNavElm && !isClicked) {
+        ovaNavElm.removeEventListener("mouseenter", handleMouseEnter);
+        ovaNavElm.removeEventListener("mouseleave", handleMouseLeave);
+        ovaNavElm.removeEventListener("click", handleClick);
+      }
+    };
+  }, [isClicked]);
 
-    <OvalNav ref={ovalNavRef}>
-        <OvalImage src="/public/Png/groupReact.png" alt="Image" />
-    </OvalNav>
-
-    </RetNav>
-</>
-)
-
-}
-
-
+  return (
+    <>
+      <ShadowOval ref={ovalShadowRef} isVisible={isShadowVisible} />
+      <RetNav ref={retNavRef}>
+        <OvalNav ref={ovalNavRef}>
+          <OvalImage src="/public/Png/groupReact.png" alt="Immagine" />
+        </OvalNav>
+      </RetNav>
+    <BackGround ref={backgroundRef}/>
+    </>
+  );
+};
 
 export default NavBar;
